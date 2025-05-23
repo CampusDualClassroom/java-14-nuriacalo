b5ddcb9491de23b3afbc59dfa8c1c1ea3e95b6c7
Nuria Calo <nuriacalomosquera@gmail.com>
2025-05-23 14:29:43 +0200

    exercice 14 solved

--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -1,11 +1,17 @@
 package com.campusdual.classroom;
 
 public class Person {
-	protected String name;
-	protected String surname;
+    protected String name;
+    protected String surname;
 
-	public Person(String name, String surname) {
-		this.name = name;
-		this.surname = surname;
-	}
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getDetails() {
+        String details = "Name: " + name + ", surname: " + surname;
+        System.out.println(details);
+        return details;
+    }
 }
--- src/main/java/com/campusdual/classroom/PoliceOfficer.java
+++ src/main/java/com/campusdual/classroom/PoliceOfficer.java
@@ -2,10 +2,17 @@ package com.campusdual.classroom;
 
 public class PoliceOfficer extends Person {
 
-	protected String squad;
+    protected String squad;
 
-	public PoliceOfficer(String name, String surname, String squad) {
-		super(name, surname);
-		this.squad = squad;
-	}
+    public PoliceOfficer(String name, String surname, String squad) {
+        super(name, surname);
+        this.squad = squad;
+    }
+
+    @Override
+    public String getDetails() {
+        String details = "PoliceOfficer. " + "Name: " + name + ", surname: " + surname + ", squad: " + squad;
+        System.out.println(details);
+        return details;
+    }
 }
